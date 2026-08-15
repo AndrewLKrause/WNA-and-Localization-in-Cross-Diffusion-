54656fe49183df7f09b39eab27d0f3966362e720
Edgardo Villar-Sepúlveda <edgardo.villar.13@sansano.usm.cl>
2026-08-15 03:43:32 +0200

    Updating pde2path files

--- pde2path files/4KS/4KS - Left/cmds.m
+++ pde2path files/4KS/4KS - Left/cmds.m
@@ -10,16 +10,17 @@ end
     keep pphome;
     p = [];
     % a, b, c, D, r, r1, r2
-    par = [0.9, 0.001, 4.28, 1.0, 0.1, 0.2, 0.5];
-    % par = [0.9, 2.0, 2.92, 1.0, 0.1, 0.2, 0.5];
+    par = [0.9, 0.0, 4.4, 1.0, 0.12, 0.2, 0.5];
+    % par = [0.9, 0.8, 2.9, 1.0, 0.12, 0.2, 0.5];
     h = 5e-2;
     p = fourKSinit(p, h, par); % also use nper=80, 120, 160, 200
     p.sw.jac = 0;
-    p.nc.dsmax = 1e-3;
+    p.nc.dsmin = 1e-20;
+    p.nc.dsmax = 5e-3;
 
 %% 2 - continue trivial branch to find BP 
     tic;
-    p = findbif(p, 10);
+    p = findbif(p, 7);
     toc
 
     % bif point 1: c = 4.29791
@@ -35,12 +36,12 @@ end
     % lssbel, switch off stuff not related to lss 
     istep = - 1e-2;
 
-    contset = 2:5;
+    contset = 1:5;
 
     for counter=contset
         p = swibra('p', char(strcat('bpt', string(counter))), char(strcat('b', string(counter))), istep);
         p.nc.dsmax = 1e-2;
-        p.nc.dsmin = 1e-10;
+        p.nc.dsmin = 1e-20;
         p.sw.spcalc = 1;
         p.sw.foldcheck = 1;
         p.sw.bifcheck = 2;
@@ -51,12 +52,13 @@ end
         t1 = toc(t1); % cont with default settings
     end
 
-    p = swibra('b1', 'bpt1', 'snake', - 1e-2);
-    p.nc.dsmin = 1e-10;
+    p = swibra('b1', 'bpt1', 'snake', - 1e-3);
+    p.nc.dsmax = 2e-3;
+    p.nc.dsmin = 1e-20;
     p.sw.verb = 2;
     p0 = p;
     t1 = tic;
-    p = cont(p, 75);
+    p = cont(p, 340);
     t1 = toc(t1); % cont with default settings
 
     % p = swibra('p', 'bpt46', 'b46', 0.02);
@@ -188,7 +190,7 @@ end
     end
     plotbra('snake', 3, 0, 'tyun', '-b', 'tyst', '-r', 'ms', 0, 'fms', 0, 'lwst', lwst, 'lwun', lwun);
     
-    xlabel('$c$', 'Interpreter', 'latex')
+    xlabel('$b$', 'Interpreter', 'latex')
     ylabel('$||u||_{L^2}$', 'Interpreter', 'latex')
     set(gca, 'fontsize', fs)
     load('snake/pt5.mat')
